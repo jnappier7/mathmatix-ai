165a333f6afad6e867cb048f416d9f359238c00e
Jason Nappier <DJ_JNape@Jasons-MBP-2.attlocal.net>
2025-04-29 18:38:53 -0400

    Fix: lowercase user model path in login route

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
