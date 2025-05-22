1154e7dd3874c6ffcaf0b9829e8b9238fc7f00c7
Jason Nappier <DJ_JNape@Jasons-MacBook-Pro-2.local>
2025-05-22 10:41:52 -0400

    🚀 Finalize Puppeteer setup with Chromium path and postinstall script

--- utils/pdf-to-image.js
+++ utils/pdf-to-image.js
@@ -4,6 +4,7 @@ const puppeteer = require("puppeteer");
 
 module.exports = async function pdfToImageBase64(pdfBuffer) {
   try {
+	process.env.PUPPETEER_EXECUTABLE_PATH = require("puppeteer").executablePath();
     const browser = await puppeteer.launch({
       headless: "new",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
