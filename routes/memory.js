7c602e6fff52b6dc62980c397da3281aa877a7df
Jason Nappier <jasonnappier@gmail.com>
2025-06-21 18:43:20 -0400

    🔧 Updated multiple files – styling, logic, and layout tweaks

--- routes/memory.js
+++ routes/memory.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Renamed function for clarity
 // MODIFIED: saveConversation now expects messageLog as an argument
